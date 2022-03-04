926b6bc2418daeb9514c8b99c3a1eaf8d72d6bf8
Afkaslan <afktly0633@hotmail.com>
2022-03-04 17:17:54 +0300

    Chat App added.

---
+++ BlackLionChat/app/src/main/java/com/afkaslan/blacklionchat/Black.kt
@@ -0,0 +1,3 @@
+package com.afkaslan.blacklionchat
+
+ data class Black(val user : String, val text : String)
---
+++ BlackLionChat/app/src/main/java/com/afkaslan/blacklionchat/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.afkaslan.blacklionchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+    }
+}
